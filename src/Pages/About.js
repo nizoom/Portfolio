11b575788e75ac1476c370d4664e9107a901b452
Nissim <nissimram1812@gmail.com>
2021-01-25 16:39:32 -0500

    Final touches to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -18,7 +18,7 @@ const About = ( props ) => {
       <div className = "main">
 
 
-          <h2 className = "par_one">
+          <h2>
            Nissim Ram is a New York based self-taught web developer. He
            looks to build tools that tackle social issues and break down barriers
            to information in ways that improve the lives of those in need. Before
@@ -27,18 +27,16 @@ const About = ( props ) => {
            brings to bear the tools to articulate complicated issues that face our
            modern world and sees the potential for technology to be a solution.
            Nissim uses front-end language fundamentals such as JavaScript, HTML,
-           and CSS to create new ways to visualize old paradigms such as the news and
-           ____ . </h2>
+           and CSS to create new ways to visualize old paradigms such as how online news
+           is delivered - with more projects to come.
 
+            <br/> <br/>
+            <div className = "par_two">
+          He is passionate about improving his coding skills and is currently
+          seeking new opportunities to do so.
+            </div>
 
-           <br/>
-           <h2 className = "par_two">
-           Nissim has also assisted with producing websites for local New York
-           creatives such as the writer _____ and curator _____ .
-           He is passionate about improving his coding skills and is currently
-           seeking new opportunities to do so.
-           </h2>
-
+          </h2>
        </div>
 
        <Languages/>
