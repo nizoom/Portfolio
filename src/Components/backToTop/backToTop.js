32fe2b1578c38c8317459c318e696537c90c4b8c
Nissim <nissimram1812@gmail.com>
2021-09-17 16:37:46 -0400

    fixed web kit btn

--- src/Components/backToTop/backToTop.js
+++ src/Components/backToTop/backToTop.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./backToTop.css"
 
-const BackToTopBtn = ( props ) => {
+const BackToTopBtn = (props) => {
 
-  const scrollUp = ( )=> {
+  const scrollUp = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth"
     })
   }
-  return(
-    <div className = "btn_div">
-      <button type = "button" className = "top"
-      onClick = {scrollUp}
+  return (
+    <div className="btn_div">
+      <button type="button" className="top"
+        onClick={scrollUp}
       > 👆 </button>
     </div>
   )
