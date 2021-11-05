4bd3fe83a4d0844cbbb425a5d25d5b2c99e39899
Nissim Ram <nissimram1812@gmail.com>
2021-11-05 14:17:06 -0400

    fixed env

--- src/Pages/apps/globalnews/src/Components/Map/getNews.js
+++ src/Pages/apps/globalnews/src/Components/Map/getNews.js
@@ -2,16 +2,10 @@
 export default async function getNews (localityName, country, topic, localityFeatureStatus) {
 
 
-
-
-
-
-//let response = fetch(`${process.env.REACT_APP_EP_STARTER}?q=${localityName} ${country} ${topic}`, {
-  //${localityName} ${country} ${topic}
 let response = fetch(`https://api.bing.microsoft.com/v7.0/news/search?q=${localityName} ${country} ${topic}`, {
   
 headers: {
-    "Ocp-Apim-Subscription-Key": "e812d12c761f48f9aa6c3973b63e9e6e",
+    "Ocp-Apim-Subscription-Key": `${process.env.REACT_APP_AZURE_KEY} `,
     'Accept': 'application/json',
     'Content-Type': 'application/json'
   }
