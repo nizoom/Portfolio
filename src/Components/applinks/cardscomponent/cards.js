8c7b35ebc87ff75d2e5921c8b054f1d4827d8188
Nissim Ram <nissimram1812@gmail.com>
2022-08-10 16:47:53 -0400

    updating front page ui. changed look of cards

--- src/Components/applinks/cardscomponent/cards.js
+++ src/Components/applinks/cardscomponent/cards.js
@@ -1,10 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import './cards.css';
-import appIconCard from "../../../project_layout/appiconcard.png"
-import appDescriptionCard from "../../../project_layout/appdescriptioncard.png"
-import DownArrow from "../../../project_layout/downarrow.png"
-import InfoPopUp from './infopopup';
-import MobileCard from "../../../project_layout/mobilecard.png"
+import Card1 from "../../../project_layout/Card1.png"
+import Card2 from "../../../project_layout/Card2.png"
 
 
 import { BrowserRouter as Router, Link } from "react-router-dom";
@@ -14,8 +11,6 @@ const Cards = (props) => {
 
 
     const handlePopupClick = (popup, boolean) => {
-    
-
     //    const updatedvalues = {
     //         status : boolean,
     //         app : popup
@@ -27,100 +22,114 @@ const Cards = (props) => {
 
 
     useEffect(() => {
-
-
         document.addEventListener("keydown", handleEscape, false)
-        
         //document.addEventListener("keydown", (event) => escapeFunction(event, props.appTitle), false)
-
-
         return () => {
             document.removeEventListener("keydown", handleEscape, false);
-
             // document.removeEventListener("click", clickOutPopup, false)
-
         }
-
-      
-
     })
 
     const handleEscape = (event) => {
-
         if(props.projectInFocus === props.appTitle){
-
             if(event.key === "Escape"){
-  
-                handlePopupClick(props.appTitle);
-                
+                handlePopupClick(props.appTitle);  
              }
-
         }
-
-    
     };
 
   
     return (
 
         // ICON CARD
-        <div className='all-cards-wrapper'>
-          
-          <img src={MobileCard} className= 'mobile-card' alt = 'card to display app on to'/>
-          <div className='columns'>
-                <div className='icon-card-wrapper'>
-                    <div className='icon-and-title-wrapper'>
-                        <h3 className="app-title"> {props.appTitle} </h3>
-
-                        <img src={DownArrow} className= 'arrow'/>
-
-                        { props.linkType === "Link" ?  <Link to="/globalnews" target="_blank" rel="noopener noreferrer">
-
-                                <img src={props.icon} alt={`${props.alt} icon`} className="app-icon" />
-
-                        </Link>  : 
-                        
-                        <a href={props.url} target="_blank" rel="noopener noreferrer"> 
-
-                            <img src={props.icon} alt={`${props.alt} icon`} className="app-icon" />
-
-                        </a>
-                        // if no link like gif situation then pass a prop to create component where onclick shows gif
-                        
-                        }
-                    
-                        </div>
-                    <img src = {appIconCard} alt = 'app icon card' className="app-icon-card"/>
-                </div>
-                
-
-                {/* DESCRIPTION CARD */}
-                <div className='description-div-wrapper'>
-                    <div className="description-text-div" >
-                        {props.Description}
-                        {/* <div className='app-description'> {props.Description}</div> */}
-                
-                        <p className="tech-used">  <span className="stack"> Tech stack: </span> <span className="tech-list"> {props.stackList} </span> </p>
-                        <button className="more-info-btn" onClick = {() => handlePopupClick(props.appTitle)}> Click here for more info</button>
+        <div className='card-component-wrapper'>
+            <div className='card-wrapper'>
+                <img src= {props.cardGraphic} className='card-img'/>
+                <section className='text-wrapper'>
+                    <div className='left-column'>
+                        <h3 className='app-title'> {props.appTitle}</h3>
+                        <button className='more-info-btn learn-more'> {props.btnOneTxt} </button>
+                        <button className='more-info-btn view-code'> View Code </button>
+                       
                     </div>
-
-
-                    <img src = {appDescriptionCard} alt = 'app description card' className="app-description-card"/>
-                </div>
-
-                {/* <div style={{width: '0px'}}></div> */}
-
+                    <div className='right-column'>
+                        <p className='app-description'> 
+                            {props.Description}
+                        </p>
+                        <p> 
+                        <span className="stack"> Tech stack: </span> 
+                            {props.stackList}
+                        </p>
+                    </div>
+                  
+                </section>
             </div>
-
-            <InfoPopUp 
             
-             appTitle = {props.appTitle} handlePopupClick = {handlePopupClick} position = {props.position} url = {props.url}
-            projectInFocus = {props.projectInFocus} status = {props.status}
-        
-            />
+            
       </div>
    
     )
 }
 
 export default Cards;
+
+
+// import appIconCard from "../../../project_layout/appiconcard.png"
+// import appDescriptionCard from "../../../project_layout/appdescriptioncard.png"
+// import DownArrow from "../../../project_layout/downarrow.png"
+// import InfoPopUp from './infopopup';
+// import MobileCard from "../../../project_layout/mobilecard.png"
+
+
+// <img src={MobileCard} className= 'mobile-card' alt = 'card to display app on to'/>
+// <div className='columns'>
+//       <div className='icon-card-wrapper'>
+//           <div className='icon-and-title-wrapper'>
+//               <h3 className="app-title"> {props.appTitle} </h3>
+
+//               <img src={DownArrow} className= 'arrow'/>
+
+//               { props.linkType === "Link" ?  <Link to="/globalnews" target="_blank" rel="noopener noreferrer">
+
+//                       <img src={props.icon} alt={`${props.alt} icon`} className="app-icon" />
+
+//               </Link>  : 
+              
+//               <a href={props.url} target="_blank" rel="noopener noreferrer"> 
+
+//                   <img src={props.icon} alt={`${props.alt} icon`} className="app-icon" />
+
+//               </a>
+//               // if no link like gif situation then pass a prop to create component where onclick shows gif
+              
+//               }
+          
+//               </div>
+//           <img src = {appIconCard} alt = 'app icon card' className="app-icon-card"/>
+//       </div>
+      
+
+//       {/* DESCRIPTION CARD */}
+//       <div className='description-div-wrapper'>
+//           <div className="description-text-div" >
+//               {props.Description}
+//               {/* <div className='app-description'> {props.Description}</div> */}
+      
+//               <p className="tech-used">  <span className="stack"> Tech stack: </span> <span className="tech-list"> {props.stackList} </span> </p>
+//               <button className="more-info-btn" onClick = {() => handlePopupClick(props.appTitle)}> Click here for more info</button>
+//           </div>
+
+
+//           <img src = {appDescriptionCard} alt = 'app description card' className="app-description-card"/>
+//       </div>
+
+//       {/* <div style={{width: '0px'}}></div> */}
+
+//   </div>
+
+//   <InfoPopUp 
+  
+//    appTitle = {props.appTitle} handlePopupClick = {handlePopupClick} position = {props.position} url = {props.url}
+//   projectInFocus = {props.projectInFocus} status = {props.status}
+
+//   />
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -3,17 +3,14 @@ import React, { Component } from "react";
 import Logo from "../Components/logo/logo"
 import Sidebar from "../Components/sidebar/sidebar"
 import "./css/homepage.css"
-import EchoIcon from "../project_layout/echochambericon.png";
-import NYCIcon from "../project_layout/builtnyclogo.png";
-import globalnewsIcon from "../project_layout/worldnews.png";
-import tyIcon from "../project_layout/tyicon.png"
-import emailBuilderIcon from "../project_layout/form.png"
 import gradientBubble from './../project_layout/Ellipse.png'
 import Cards from "../Components/applinks/cardscomponent/cards"
 import Socials from "../Components/socials/socials";
-import TwitterIcon from '../project_layout/twitter.png'
 import  { firstVisit, getLocalVisitStatus} from "../Components/localstorage/localstorage";
-
+import Card1 from "../project_layout/Card1.png"
+import Card2 from "../project_layout/Card2.png"
+import Card3 from "../project_layout/Card3.png"
+import Card4 from "../project_layout/Card4.png"
 
 
 class HomePage extends Component {
@@ -126,43 +123,50 @@ class HomePage extends Component {
             <section className="apps-section">
 
           
-             <Cards alt = 'TY Email Builder' icon = {emailBuilderIcon} appTitle = 'TY Email Builder' stackList = ' ReactJS, ExpressJS, NodeJS, NodeMailer, and Handlebars.'
-                linkType = 'href' url = 'https://github.com/nizoom/TYTemplateBuilder'
-                Description = {<p className='app-description'>  <b>Form web app </b> that builds and sends templated thank you emails.</p>}
+             <Cards alt = 'Thank You Email Generator'  appTitle = 'Thank You Email Generator' stackList = ' ReactJS, ExpressJS, NodeJS, NodeMailer, and Handlebars.'
+                linkType = 'href' githubUrl = 'https://github.com/nizoom/TYTemplateBuilder'
+                Description = {<p className='app-description'> Form web app that builds and sends templated thank you emails.</p>}
                 toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'one' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                cardGraphic={Card1} btnOneTxt='View GIF'
               /> 
 
-              <Cards alt = 'Twitter API Clone' icon = {TwitterIcon} appTitle = 'Twitter API Clone' stackList = 'Java, SpringBoot, JPA, PostgresQL, PGAdmin, Postman, Eclipse, Maven'
-                Description = {<p className='app-description'>  <b>Java backend</b> including endpoints to create users, like tweets, and follow accounts</p>}
-                toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'one' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
-                url = 'https://github.com/nizoom/Twitter-API-Clone'
-              /> 
-
-              <Cards alt = 'Build NYC'icon = {NYCIcon} appTitle = 'Try Build NYC' stackList = 'React JS, LeafletJS, ChartJS, SoQueryLanguage, NodeJS, ExpressJS.'
-                linkType = 'href' url = 'https://build-nyc.herokuapp.com/' 
-                Description ={ <p className='app-description'> <b> Data visualization interface </b>  that illustrates building development in NYC. </p> }
+              <Cards alt = 'Twitter API Clone'  appTitle = 'Twitter API Clone' stackList = 'Java, SpringBoot, JPA, PostgresQL, PGAdmin, Postman, Eclipse, Maven'
+                Description = {<p className='app-description'>  Java backend including endpoints to create users, like tweets, and follow accounts</p>}
                 toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'two' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                githubUrl = 'https://github.com/nizoom/Twitter-API-Clone'
+                cardGraphic={Card2} btnOneTxt='Learn more'
               /> 
 
-              <Cards alt = 'TY Email Automation'icon = {tyIcon} appTitle = 'TY Email Automation' stackList = ' ExpressJS, NodeJS, NodeMailer, and Handlebars.'
-                linkType = 'href' url = 'https://github.com/nizoom/TYAutomation'
-                Description = {<p className='app-description'>  <b>Backend app </b> that uses that generates custom daily thank you emails based on donations from an API.</p>}
+              <Cards alt = 'Build NYC' appTitle = 'Build NYC' stackList = 'React JS, LeafletJS, ChartJS, SoQueryLanguage, NodeJS, ExpressJS.'
+                linkType = 'href' url = 'https://build-nyc.herokuapp.com/' 
+             
+                Description ={ <p className='app-description'> Data visualization interface that illustrates building development in NYC. </p> }
                 toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'three' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                cardGraphic={Card3} btnOneTxt='Use'
               /> 
 
-              <Cards alt = 'Global News'icon = {globalnewsIcon} appTitle = 'Try Global News'
+              <Cards alt = 'Thank you Email Automation' appTitle = 'TY Email Automation' stackList = ' ExpressJS, NodeJS, NodeMailer, and Handlebars.'
+                linkType = 'href' url = 'https://github.com/nizoom/TYAutomation'
+                Description = {<p className='app-description'> Backend app that uses that generates custom daily thank you emails based on donations from an API.</p>}
+                toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'four' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                cardGraphic={Card4} btnOneTxt='Learn more'
+              /> 
+
+              <Cards alt = 'Global News' appTitle = 'Global News'
                 url = 'http://localhost:3000/#/globalnews'
                 stackList = 'ReactJS, React Router, Material UI, Azure, Google Cloud, LeafletJS.'
                 linkType = 'Link' 
-                Description = {<p className='app-description'> <b>Web app </b> that finds localized news based on geolocation from map click.</p>}
-                toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'four' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                Description = {<p className='app-description'> Web app that finds localized news based on geolocation from map click.</p>}
+                toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'five' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                cardGraphic={Card1} btnOneTxt='Use'
               />
          
              
-              <Cards alt = 'Echo Chamber'icon = {EchoIcon} appTitle = 'Try Echochamber' stackList = 'ReactJS, Material UI, NodeJS, ExpressJS.'
+              <Cards alt = 'Echo Chamber' appTitle = 'Echochamber' stackList = 'ReactJS, Material UI, NodeJS, ExpressJS.'
                 linkType = 'href' url = 'https://entertheechochamber.herokuapp.com/'
-                Description = {<p className='app-description'>  <b>Web scraping app </b> that uses Twitter's  API to recreate a personal feed.</p>}
-                toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'five' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                Description = {<p className='app-description'> Web scraping app that uses Twitter's  API to recreate a personal feed.</p>}
+                toggleOpacityForPopup ={this.state.toggleOpacityForPopup} position = 'six' projectInFocus = {this.state.projectInFocus} status = {this.state.popupStatus}
+                cardGraphic={Card2} btnOneTxt='Use'
               /> 
         
 
@@ -182,3 +186,10 @@ class HomePage extends Component {
 
 export default HomePage;
 
+
+// import EchoIcon from "../project_layout/echochambericon.png";
+// import NYCIcon from "../project_layout/builtnyclogo.png";
+// import globalnewsIcon from "../project_layout/worldnews.png";
+// import tyIcon from "../project_layout/tyicon.png"
+// import emailBuilderIcon from "../project_layout/form.png"
+// import TwitterIcon from '../project_layout/twitter.png'
