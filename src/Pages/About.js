3d326cfea19521ebfabd7e932c37e542c6d651a9
Nissim Ram <nissimram1812@gmail.com>
2022-08-12 14:02:50 -0400

    updated about page. finished new cards on front page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -57,18 +57,3 @@ const About = (props) => {
 
 export default About;
 
-
-// I love to break down barriers to information and build tools that tackle important issues. I've found that web development can be a great way
-// to empower users through intuitive UI design, data
-// visualization, and employing APIs to bring up to date information.
-
-// Before entering the tech world I graduated from Brandeis University with a degree in
-// history and anthropology.
-
-// This background enables me to think about the unique challenges of our time and how
-// technology can be used in service of those issues.
-
-// I use the front-end language fundamentals of JavaScript, HTML, and CSS
-// to create new ways to visualize old paradigms such as how online news
-// is delivered, how we can view Twitter through someone else's eyes, and much more. My projects and client work are created using modern web development tools such as React,
-// Express JS, and Flexbox.
