8208d358c26331217775a3a48763e5cf66a2fa15
nizoom <nissimram1812@gmail.com>
2024-02-18 18:19:49 -0500

    added on hover logo animation

--- src/Components/logo/logo.js
+++ src/Components/logo/logo.js
@@ -1,26 +1,35 @@
 import "./logo.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import NRLogo from "../../project_layout/nrlogo.svg";
 
 const Logo = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    if (!isHovered) {
+      console.log("mouse enter");
+      setIsHovered(true);
+      setTimeout(() => {
+        setIsHovered(false);
+      }, 4000);
+    }
+  };
+
   return (
     <div className="wrapper">
       <Link to="/" style={{ textDecoration: "none" }}>
-        <img
-          src={NRLogo}
-          style={{
-            maxWidth: "50%",
-            maxHeight: "50%",
-            margin: "20px auto auto 20px",
-          }}
-        />
-
-        {/* <div className="name-div">
-          <h1 className="gov_name"> Nissim Ram </h1>
-          <hr className="gold-bar" />
-          <h2 className="main-sub-header"> web developer</h2>
-        </div> */}
+        <div className={isHovered ? "rotate-logo" : ""}>
+          <img
+            onMouseEnter={handleMouseEnter}
+            src={NRLogo}
+            style={{
+              maxWidth: "32%",
+              maxHeight: "50%",
+              margin: "20px auto auto 20px",
+              transform: "translateY(calc(25px * var(--x))) translateZ(0px)",
+            }}
+          />
+        </div>
       </Link>
     </div>
   );
