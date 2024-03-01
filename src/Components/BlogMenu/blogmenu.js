6ecbc26b5787f3d0f136ef0e6228c6c460546754
nizoom <nissimram1812@gmail.com>
2024-03-01 13:57:45 -0500

    added coming soon animatino to label insead of message

--- src/Components/BlogMenu/blogmenu.js
+++ src/Components/BlogMenu/blogmenu.js
@@ -1,37 +1,59 @@
 import React, { useState } from "react";
 import "../../Pages/css/blog.css";
-import { useMediaQuery } from "react-responsive";
 import { Link } from "react-router-dom";
 const BlogMenu = (props) => {
   // const isDesktop = useMediaQuery({ minWidth: 1024 });
-  // const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
-  const isMobile = useMediaQuery({ maxWidth: 768 });
 
   const [comingSoonMsgClass, setComingSoonMsgClass] = useState(
     "hide-coming-soon-msg"
   );
 
-  const handleComingSoonMsgClass = () => {
-    if (comingSoonMsgClass === "hide-coming-soon-msg") {
-      if (isMobile) {
-        setComingSoonMsgClass("mobile-coming-soon-msg");
-      } else {
-        setComingSoonMsgClass("show-coming-soon-msg");
-      }
-      const timer = setTimeout(() => {
-        setComingSoonMsgClass("hide-coming-soon-msg");
-      }, 2000);
+  const [blogLabelState, setBlogLabelState] = useState({
+    music: { text: "Music", class: "default-label-class" },
+    tech: { text: "Tech", class: "default-label-class" },
+    arsenal: { text: "Arsenal", class: "default-label-class" },
+    writing: { text: "Writing", class: "default-label-class" },
+  });
 
-      // Clean up the timer on component unmount
-      return () => clearTimeout(timer);
-    }
-  };
-
-  const toggleBlogPage = (page) => {
-    handleComingSoonMsgClass();
+  const toggleLableStatus = (page) => {
+    // handleComingSoonMsgClass();
     props.returnSelectedPgComponent(page);
+    page = page.toLowerCase();
+
+    const animations = [
+      { func: slideOut, args: [page], delay: 0 },
+      { func: slideIn, args: [page, "Coming Soon"], delay: 1000 },
+      { func: slideOut, args: [page], delay: 2000 },
+      { func: slideIn, args: [page], delay: 3000 },
+    ];
+
+    animations.forEach(({ func, args, delay }) => {
+      setTimeout(() => {
+        func(...args);
+      }, delay);
+    });
   };
 
+  const updateLabelClass = (page, cssClass) => {
+    setBlogLabelState((prevState) => ({
+      ...prevState,
+      [page]: { ...prevState[page], class: cssClass },
+    }));
+  };
+  const updateLabelClassAndText = (page, cssClass, text = page) => {
+    setBlogLabelState((prevState) => ({
+      ...prevState,
+      [page]: { text: String(text), class: cssClass },
+    }));
+  };
+  const slideOut = (page) => {
+    updateLabelClass(page, "slide-out");
+  };
+
+  const slideIn = (page, text = page) => {
+    const capitalizedText = text.charAt(0).toUpperCase() + text.slice(1);
+    updateLabelClassAndText(page, "slide-in", capitalizedText);
+  };
   return (
     <div className="blog-menu-flexbox">
       <div className="blog-page-description-wrapper">
@@ -48,9 +70,11 @@ const BlogMenu = (props) => {
         <ul>
           <li
             className="blog-tab tech-tab"
-            onClick={() => toggleBlogPage("Tech")}
+            onClick={() => toggleLableStatus("Tech")}
           >
-            <h3>Tech</h3>
+            <h3 className={blogLabelState.tech.class}>
+              {blogLabelState.tech.text}
+            </h3>
           </li>
 
           <li
@@ -67,15 +91,19 @@ const BlogMenu = (props) => {
 
           <li
             className="blog-tab music-tab"
-            onClick={() => toggleBlogPage("Music")}
+            onClick={() => toggleLableStatus("Music")}
           >
-            <h3>Music</h3>
+            <h3 className={blogLabelState.music.class}>
+              {blogLabelState.music.text}
+            </h3>
           </li>
           <li
             className="blog-tab writing-tab"
-            onClick={() => toggleBlogPage("Writing")}
+            onClick={() => toggleLableStatus("Writing")}
           >
-            <h3>Writing</h3>
+            <h3 className={blogLabelState.writing.class}>
+              {blogLabelState.writing.text}
+            </h3>
           </li>
         </ul>
       </nav>
